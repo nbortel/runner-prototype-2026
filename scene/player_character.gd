20ba7d6fa8e212b6b7af7c837abadd7ebb8bf438
Noah Bortel <nbortel98@gmail.com>
2026-03-24 19:01:24 -0700

    feat: basic player character movement

---
+++ scene/player_character.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+const VELOCITY_DELTA = 3.0
+const BASE_SPEED: int = 100.0
+const FORWARD_SPEED_MODIFIER = 25.0
+const BACKWARD_SPEED_MODIFIER = 25.0
+
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	# Handle Horizontal Movement
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction == 1:
+		velocity.x = move_toward(velocity.x, BASE_SPEED + FORWARD_SPEED_MODIFIER, VELOCITY_DELTA)
+	elif direction == -1:
+		velocity.x = move_toward(velocity.x, BASE_SPEED - BACKWARD_SPEED_MODIFIER, VELOCITY_DELTA)
+	else:
+		velocity.x = move_toward(velocity.x, BASE_SPEED, VELOCITY_DELTA)
+	move_and_slide()
